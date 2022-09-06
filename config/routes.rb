5896cde3ab37bae27b5997795085acda37529515
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2022-09-06 11:06:03 -0400

    Added articles, controller, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "about#show"
+  resources :articles
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
